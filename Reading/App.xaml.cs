12ac213c356814faf7694a88e0527bfaea435ebb
sasithekings <46295800+sasithekings@users.noreply.github.com>
2023-12-01 16:25:52 -0600

    Add project files.

---
+++ Reading/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Reading
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Reading/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Reading
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
