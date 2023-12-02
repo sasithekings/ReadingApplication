970eab97e4d0e0de6f71a78eee56b0f171d3a301
sasithekings <46295800+sasithekings@users.noreply.github.com>
2023-12-01 21:09:39 -0600

    added addpage view model and method to insert data into the database

--- Reading/MainViewModel.cs
+++ Reading/MainViewModel.cs
@@ -28,7 +28,6 @@ namespace Reading
 
     }
 
-
     public class ViewPageViewModel : BindableObject
     {
         public ObservableCollection<DataEntry> DataEntrys { get; set; } = new ObservableCollection<DataEntry>();
@@ -46,6 +45,44 @@ namespace Reading
     }
 
 
+
+
+    public class AddPageViewModel : BindableObject
+    {
+        private string _title;
+        private string _content;
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set
+            {
+                _content = value;
+                OnPropertyChanged(nameof(Content));
+            }
+        }
+
+        public ICommand SubmitCommand => new Command(ExecuteSubmitCommand);
+
+        private void ExecuteSubmitCommand()
+        {
+            var newItem = new DataEntry { title = Title, content = Content };
+            var service = new ReadingDataBase();
+            service.InsertDataItem(newItem);
+
+            Application.Current.MainPage.Navigation.PopAsync();
+        }
+    }
 
 }
 
