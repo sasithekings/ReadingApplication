8f975227dcd5194d542b173d9d16f00203fbaddb
sasithekings <46295800+sasithekings@users.noreply.github.com>
2023-12-01 20:54:33 -0600

    binding the view page view model to the view page

--- Reading/ViewPage.xaml.cs
+++ Reading/ViewPage.xaml.cs
@@ -1,12 +1,14 @@
-﻿namespace Reading
+﻿
+namespace Reading
 {
     public partial class ViewPage : ContentPage
     {
         public ViewPage()
         {
             InitializeComponent();
+            BindingContext = new ViewPageViewModel();
         }
-
     }
+
 
 }
