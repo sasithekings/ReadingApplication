c35d414647e4263eead4873ce25f29244942797d
sasithekings <46295800+sasithekings@users.noreply.github.com>
2023-12-01 20:28:36 -0600

    Created and assigned MVVM for mainpage

---
+++ Reading/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Reading
+{
+    public class MainViewModel : BindableObject
+    {
+        public ObservableCollection<DataEntry> DataItems { get; set; } = new ObservableCollection<DataEntry>();
+
+        public ICommand ViewPageCommand => new Command(ExecuteViewPageCommand);
+        public ICommand AddPageCommand => new Command(ExecuteAddPageCommand);
+
+        private void ExecuteViewPageCommand()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new ViewPage());
+        }
+
+        private void ExecuteAddPageCommand()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new AddPage());
+        }
+
+
+    }
+   
+
+}
+
+
