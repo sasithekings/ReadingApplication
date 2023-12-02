69ece38a19c5d74ecef4a36bb877e7470fdcdcea
sasithekings <46295800+sasithekings@users.noreply.github.com>
2023-12-01 19:22:02 -0600

    using sqlite created a database

---
+++ Reading/ReadingDataBase.cs
@@ -0,0 +1,19 @@
+﻿
+using SQLite;
+using System.Collections.ObjectModel;
+
+namespace Reading
+{
+    public class ReadingDataBase
+    {
+        SQLiteConnection database;
+
+        public ReadingDataBase()
+        {
+            var databasePath = System.IO.Path.Combine(FileSystem.AppDataDirectory, "Database.db3");
+            database = new SQLiteConnection(databasePath);
+            database.CreateTable<DataEntry>();
+        }
+
+    }
+}
